b9d6edb98ec0db542b9dc3e44c08cbacf60e7a9e
Shubhamkumar-op <singhshubhamkumar778@gmail.com>
2025-03-23 03:42:39 +0530

    Deploying to Railway

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,16 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from routes import router
-
 
 app = FastAPI()
 app.include_router(router)
 
 @app.get("/")
 def root():
-    return {"message": "LLM Booking System API is running!"}
+    return {"message": "FastAPI is running on Railway!"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    print(f"Running FastAPI on PORT: {port}")
+    uvicorn.run(app, host="0.0.0.0", port=port)
